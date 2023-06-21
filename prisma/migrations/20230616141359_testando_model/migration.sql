8efd29d1a306b2d1aafb86c64dcb8d0fd2269415
cti-st2 <cti-st2@cti-st2.sedam.local>
2023-06-21 13:28:52 -0400

    Normalização

--- prisma/migrations/20230616141359_testando_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `arquivo` on the `requerimento` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `requerimento` DROP COLUMN `arquivo`;
--- prisma/migrations/20230616142232_testando_model/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `arquivo` to the `Requerimento` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `requerimento` ADD COLUMN `arquivo` MEDIUMBLOB NOT NULL;
